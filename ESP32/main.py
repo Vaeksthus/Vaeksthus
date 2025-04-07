3e059e7bee11e0070d211d9fe16d9d3b9314765f
Fostercare123 <niko.vasilije@gmail.com>
2025-04-07 12:52:31 +0200

    Opdateret

--- ESP32/main.py
+++ ESP32/main.py
@@ -1,11 +1,12 @@
 from time import sleep
-
 from relay import Relay
 from temperatur87 import LMT87
+from float_switch import Flydekontakt  # Import af flydekontakten
 
 # Konfiguration af pins
 PIN_LMT87 = 35
 PIN_RELAY = 26
+PIN_FLOAT = 34  # GPIO til flydekontakten
 
 # Kalibreringsværdier for LMT87
 T1, ADC1 = 25.2, 2659
@@ -15,20 +16,40 @@ def main():
     """Hovedfunktion der overvåger temperatur og styrer relæet."""
     relay = Relay(PIN_RELAY)
     sensor = LMT87(PIN_LMT87)
+    flyder = Flydekontakt(PIN_FLOAT)  # Initialiser flydekontakten
+
     sensor.calibrate(T1, ADC1, T2, ADC2)
+
+    # Gem tidligere status for flydekontakten for at spore ændringer
+    sidste_vandstatus = flyder.er_aktiveret()
 
     while True:
         temperature = sensor.get_temperature()
         relay_status = "NO" if relay.relay.value() else "NC"
+        aktuel_vandstatus = flyder.er_aktiveret()
 
-        if temperature > 18:
+        # Omvendt logik for vandniveau
+        vand_status = "Lavt" if aktuel_vandstatus else "Højt"
+
+        # Temperaturstyret relæ
+        if temperature > 23:
             relay.on()
             relay_status = "NO"
         else:
             relay.off()
             relay_status = "NC"
 
-        print("Temperatur: {:.2f}°C | Relæ: {}".format(temperature, relay_status))
+        # Print ændring i flydekontakt
+        if aktuel_vandstatus != sidste_vandstatus:
+            if aktuel_vandstatus:
+                print("💧 Flydekontakt aktiveret – vandniveau er LAVT.")
+            else:
+                print("⚠️  Flydekontakt deaktiveret – vandniveau er HØJT.")
+
+        print("Temperatur: {:.2f}°C | Relæ: {} | Vandniveau: {}".format(
+            temperature, relay_status, vand_status))
+
+        sidste_vandstatus = aktuel_vandstatus  # Opdater status
         sleep(1)
 
 # Hvis filen køres direkte, start hovedfunktionen
